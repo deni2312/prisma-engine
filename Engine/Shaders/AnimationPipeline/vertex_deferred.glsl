09ea31dbd118e73451abc8fd96216e92956b6e86
denis.beqiraj <denis.beqiraj@supsi.ch>
2024-05-11 13:23:55 +0200

    Added bitangent

--- Engine/Shaders/AnimationPipeline/vertex_deferred.glsl
+++ Engine/Shaders/AnimationPipeline/vertex_deferred.glsl
@@ -3,7 +3,7 @@ layout(location = 0) in vec3 aPos;
 layout(location = 1) in vec3 aNormal;
 layout(location = 2) in vec2 aTexCoords;
 layout(location = 3) in vec3 aTangent;
-layout(location = 4) in vec2 aBitangent;
+layout(location = 4) in vec3 aBitangent;
 layout(location = 5) in ivec4 boneIds;
 layout(location = 6) in vec4 weights;
 
--- Engine/Shaders/DeferredPipeline/vertex.glsl
+++ Engine/Shaders/DeferredPipeline/vertex.glsl
@@ -3,7 +3,7 @@ layout(location = 0) in vec3 aPos;
 layout(location = 1) in vec3 aNormal;
 layout(location = 2) in vec2 aTexCoords;
 layout(location = 3) in vec3 aTangent;
-layout(location = 4) in vec2 aBitangent;
+layout(location = 4) in vec3 aBitangent;
 
 out vec3 FragPos;
 out vec3 viewPos;
