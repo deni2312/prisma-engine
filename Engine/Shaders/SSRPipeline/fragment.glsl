b39a5d59ef32ae3acd61112086e49c3c41b1db5c
denis.beqiraj <denis.beqiraj@supsi.ch>
2024-05-17 16:21:43 +0200

    Added reflection

--- Engine/Shaders/SSRPipeline/fragment.glsl
+++ Engine/Shaders/SSRPipeline/fragment.glsl
@@ -8,79 +8,46 @@ layout(bindless_sampler) uniform sampler2D textureAlbedo;
 layout(bindless_sampler) uniform sampler2D textureNorm;
 layout(bindless_sampler) uniform sampler2D texturePosition;
 
-layout(std140, binding = 1) uniform MeshData
-{
+layout(std140, binding = 1) uniform MeshData {
     mat4 view;
     mat4 projection;
 };
 
 uniform vec2 screenSize; // Screen size
 
-// Function to perform ray marching
-vec3 rayMarch(vec3 startPos, vec3 rayDir) {
-    const int maxSteps = 50; // Max steps for ray marching
-    const float stepSize = 0.1; // Step size
-    vec3 pos = startPos;
+const int numSamples = 128;
+const float inverseResolution = 0.001; // Hardcoded ratio.x value
 
-    for (int i = 0; i < maxSteps; i++) {
-        pos += rayDir * stepSize;
+void main(void)
+{
+    vec4 color = vec4(0.0);
+    vec4 surfaceData = texture(textureNorm, UV);
+    float depth = surfaceData.w;
 
-        // Project the position back to screen space
-        vec4 projPos = projection * vec4(pos, 1.0);
-        projPos /= projPos.w;
-        vec2 screenUV = (projPos.xy * 0.5) + 0.5;
+    vec3 eyeSpaceNormal = normalize(surfaceData.xyz);
+    vec3 eyeSpacePosition = normalize(texture(texturePosition, UV).xyz);
 
-        // Check if the position is within screen bounds
-        if (screenUV.x < 0.0 || screenUV.x > 1.0 || screenUV.y < 0.0 || screenUV.y > 1.0) {
+    vec4 viewDir = inverse(projection) * vec4(0.0, 0.0, 1.0, 1.0);
+    viewDir /= viewDir.w;
+
+    vec4 reflectionVector = normalize(projection * vec4(reflect(viewDir.xyz, eyeSpaceNormal), 1.0));
+    float refLength = length(reflectionVector.xy);
+    reflectionVector = normalize(reflectionVector) / refLength * inverseResolution;
+    reflectionVector.z *= depth;
+
+    vec3 currentPosition = vec3(reflectionVector.x, reflectionVector.y, depth + reflectionVector.z);
+
+    int currentSamples = 0;
+    while (currentSamples < numSamples) {
+        float sampledDepth = texture(textureNorm, UV + currentPosition.xy).w;
+        float difference = currentPosition.z - sampledDepth;
+        if (difference > 0.0025) {
+            color = texture(textureAlbedo, UV + currentPosition.xy);
             break;
         }
-
-        // Sample the depth buffer at this screen position
-        float sampledDepth = texture(texturePosition, screenUV).z;
-
-        // If the sampled depth is greater than the current position's depth, we have hit a surface
-        if (sampledDepth < pos.z) {
-            return pos;
-        }
+        currentPosition += reflectionVector.xyz;
+        currentSamples++;
     }
 
-    // If no hit is found, return a large value to indicate no intersection
-    return vec3(1e8);
-}
-
-void main() {
-    // Sample textures
-    vec4 albedo = texture(textureAlbedo, UV);
-    vec3 normal = texture(textureNorm, UV).xyz;
-    vec3 position = texture(texturePosition, UV).xyz;
-
-    // Extract metalness from albedo's alpha channel
-    float metalness = albedo.a;
-
-    // View direction
-    vec3 viewDir = normalize(position - vec3(view[3]));
-
-    // Reflect view direction around the normal
-    vec3 reflectionDir = reflect(viewDir, normal);
-
-    // Perform ray marching to find the reflected position
-    vec3 reflectedPos = rayMarch(position, reflectionDir);
-
-    // Check if reflection hit something
-    if (reflectedPos.z < 1e8) {
-        // Project the reflected position to screen space
-        vec4 reflectedProjPos = projection * vec4(reflectedPos, 1.0);
-        reflectedProjPos /= reflectedProjPos.w;
-        vec2 reflectedUV = (reflectedProjPos.xy * 0.5) + 0.5;
-
-        // Sample the albedo at the reflected position
-        vec4 reflectedColor = texture(textureAlbedo, reflectedUV);
-
-        // Mix the reflected color with the original albedo based on metalness
-        outColor = mix(vec4(albedo.xyz,1.0), vec4(reflectedColor.xyz,1.0), metalness);
-    }
-    else {
-        // If no reflection, just use the original albedo
-        outColor = vec4(albedo.xyz,1.0);
-    }
+    outColor = vec4(color.rgb, 1.0);
 }
