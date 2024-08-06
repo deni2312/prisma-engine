2f3cbc6eb9fb2724512940f88a85e8b5bb35526c
denis <denis@DENI>
2024-08-06 19:17:37 +0200

    Added prepass

---
+++ Engine/Shaders/PrePassPipeline/vertex.glsl
@@ -0,0 +1,19 @@
+#version 460 core
+layout(location = 0) in vec3 aPos;
+
+layout(std140, binding = 1) uniform MeshData
+{
+    mat4 view;
+    mat4 projection;
+};
+
+layout(std430, binding = 1) readonly buffer Matrices
+{
+    mat4 modelMatrices[];
+};
+
+void main()
+{
+    FragPos = vec3(modelMatrices[gl_DrawID] * vec4(aPos, 1.0));
+    gl_Position = projection * view * vec4(FragPos, 1.0);
+}
