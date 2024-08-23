710457020e56eaefc6b8dd5db54fb262104228f7
denis <denis@DENI>
2024-08-23 13:59:07 +0200

    Added renders

--- Engine/src/Pipelines/PipelineCloud.cpp
+++ Engine/src/Pipelines/PipelineCloud.cpp
@@ -1,7 +1,12 @@
 #include "../../include/Pipelines/PipelineCloud.h"
 
+static std::shared_ptr<Prisma::Shader> shader = nullptr;
+
 Prisma::PipelineCloud::PipelineCloud()
 {
+	if (!shader) {
+		shader = std::make_shared<Shader>("../../../Engine/Shaders/CloudPipeline/vertex.glsl", "../../../Engine/Shaders/CloudPipeline/fragment.glsl");
+	}
 }
 
 void Prisma::PipelineCloud::render()
