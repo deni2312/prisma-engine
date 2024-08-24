b26abd46ba4cb13f3d50d1459efeef6262c23d29
denis <denis@DENI>
2024-08-24 17:28:15 +0200

    Added clouds

--- Engine/Shaders/CloudPipeline/vertex.glsl
+++ Engine/Shaders/CloudPipeline/vertex.glsl
@@ -1,18 +1,11 @@
 #version 460 core
-layout(location = 0) in vec3 aPos;
+layout(location = 0) in vec2 aPos;
+layout(location = 1) in vec2 aTexCoords;
 
-out vec3 WorldPos;
-
-layout(std140, binding = 1) uniform MeshData
-{
-    mat4 view;
-    mat4 projection;
-};
-
-uniform mat4 model;
+out vec2 TexCoords;
 
 void main()
 {
-    WorldPos = aPos;
-    gl_Position = projection * view * model * vec4(WorldPos, 1.0);
+    TexCoords = aTexCoords;
+    gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0);
 }
