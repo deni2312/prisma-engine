fb3c36c34eea4c494aa8d09b99650dcf2a976776
denis <denis@DENI>
2024-10-18 12:06:07 +0200

    Added debugger

--- Engine/include/Physics/DrawDebugger.h
+++ Engine/include/Physics/DrawDebugger.h
@@ -1,27 +1,17 @@
 #pragma once
 #include "Line.h"
-#include "btBulletDynamicsCommon.h"
+#include <Jolt/Jolt.h>
+#include <Jolt/Renderer/DebugRendererSimple.h>
 
 namespace Prisma {
 
-    class DrawDebugger : public btIDebugDraw {
+    class DrawDebugger : public JPH::DebugRendererSimple{
     public:
         Line line;
-
-        void drawLine(const btVector3 &from, const btVector3 &to, const btVector3 &color) override;
-
-        void drawContactPoint(const btVector3 &PointOnB, const btVector3 &normalOnB, btScalar distance, int lifeTime,
-                              const btVector3 &color) override;
-
-        void reportErrorWarning(const char *warningString) override;
-
-        void draw3dText(const btVector3 &location, const char *textString) override;
-
-        void setDebugMode(int debugMode) override;
-
-        int getDebugMode() const override;
-
+        virtual void DrawLine(JPH::RVec3Arg inFrom, JPH::RVec3Arg inTo, JPH::ColorArg inColor) override;
+        virtual void DrawText3D(JPH::RVec3Arg inPosition, const std::string_view& inString, JPH::ColorArg inColor, float inHeight) override;
     private:
         int debug;
+
     };
 }
--- Engine/src/Physics/DrawDebugger.cpp
+++ Engine/src/Physics/DrawDebugger.cpp
@@ -1,41 +1,19 @@
 
 #include "../../include/Physics/DrawDebugger.h"
+#include "../../include/Helpers/PrismaMath.h"
 
-void Prisma::DrawDebugger::drawLine(const btVector3 &from, const btVector3 &to, const btVector3 &color) {
-    glm::vec3 from_object;
-    from_object.x=from.getX();
-    from_object.y=from.getY();
-    from_object.z=from.getZ();
-    glm::vec3 to_object;
-    to_object.x=to.getX();
-    to_object.y=to.getY();
-    to_object.z=to.getZ();
+void Prisma::DrawDebugger::DrawLine(JPH::RVec3Arg inFrom, JPH::RVec3Arg inTo, JPH::ColorArg inColor)
+{
+    glm::vec3 from_object=Prisma::JfromVec3(inFrom);
+    glm::vec3 to_object = Prisma::JfromVec3(inTo);
     glm::vec3 color_object;
-    color_object.x=color.getX();
-    color_object.y=color.getY();
-    color_object.z=color.getZ();
+    color_object.x = inColor.r;
+    color_object.y = inColor.g;
+    color_object.z = inColor.b;
     line.setColor(color_object);
-    line.draw(from_object,to_object);
+    line.draw(from_object, to_object);
 }
 
-void
-Prisma::DrawDebugger::drawContactPoint(const btVector3 &PointOnB, const btVector3 &normalOnB, btScalar distance, int lifeTime,
-                               const btVector3 &color) {
-
-}
-
-void Prisma::DrawDebugger::reportErrorWarning(const char *warningString) {
-    std::cout<<*warningString;
-}
-
-void Prisma::DrawDebugger::draw3dText(const btVector3 &location, const char *textString) {
-
-}
-
-void Prisma::DrawDebugger::setDebugMode(int debugMode) {
-    debug=debugMode;
-}
-
-int Prisma::DrawDebugger::getDebugMode() const {
-    return debug;
+void Prisma::DrawDebugger::DrawText3D(JPH::RVec3Arg inPosition, const std::string_view& inString, JPH::ColorArg inColor, float inHeight)
+{
 }
