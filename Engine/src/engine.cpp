fd2524e875954e721d98a9484cdb267011f06ec7
denis <denis@DENI>
2024-11-01 20:44:45 +0100

    Added instances

--- Engine/src/engine.cpp
+++ Engine/src/engine.cpp
@@ -105,12 +105,13 @@ bool Prisma::Engine::run()
 			data->lastTime = currentTime;
 			data->fps = 1.0f / deltaTime.count();
 			PrismaFunc::getInstance().clear();
-
-			data->userData->update();
-			ComponentsHandler::getInstance().updateStart();
-			ComponentsHandler::getInstance().updateComponents();
-			Physics::getInstance().update(1.0f / fps());
-
+			if (!data->debug)
+			{
+				data->userData->update();
+				ComponentsHandler::getInstance().updateStart();
+				ComponentsHandler::getInstance().updateComponents();
+				Physics::getInstance().update(1.0f / fps());
+			}
 			data->sceneHandler->onBeginRender();
 			if (data->debug)
 			{
