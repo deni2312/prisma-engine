d47504c210333a0e26f5f6c8c893db27fd8297b0
denis <denis@DENI>
2024-11-09 13:40:15 +0100

    Added groups

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "UserEngine/include/UserEngine.h"
-int main() {
+
+int main()
+{
 	auto userEngine = std::make_shared<UserEngine>();
 	Prisma::Engine::getInstance().setUserEngine(userEngine);
 	Prisma::Engine::getInstance().setGuiData(Prisma::ImguiDebug::getInstance().handlers());
