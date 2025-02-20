f6537c3eea15658896405d9a14845e84197f49f8
denis <denis@DENI>
2025-02-20 20:53:35 +0100

    Added groups

--- UserEngine/Shaders/TerrainPipeline/fragment.glsl
+++ UserEngine/Shaders/TerrainPipeline/fragment.glsl
@@ -33,6 +33,7 @@ layout(std140, binding = 1) uniform MeshData
 #include ../../../Engine/Shaders/HelperHeaderPipeline/light_func.glsl
 #include ../../../Engine/Shaders/PbrHeaderPipeline/pbr_func.glsl
 #include ../../../Engine/Shaders/ShadowHeaderPipeline/shadow_func.glsl
+#include ../../../Engine/Shaders/PbrHeaderPipeline/pbr_calculation.glsl
 
 
 vec3 getNormalFromMap(vec3 tangentNormal)
@@ -102,119 +103,6 @@ void main()
 
     vec3 N = normalize(blendedNormal);  // Use the blended normal in lighting calculations
 
-    vec3 albedo = vec3(blendedColor);
-    float metallic = 0.0;
-    float roughness = blendedRoughness;
-
-    vec3 V = normalize(viewPos.xyz - FragPos);
-
-    vec3 F0 = vec3(0.04);
-    F0 = mix(F0, albedo, metallic);
-
-    vec3 Lo = vec3(0.0);
-
-    vec3 R = reflect(-V, N);
-
-    for (int i = 0; i < lenDir.r; i++) {
-
-        vec3 L = normalize(vec3(directionalData[i].direction));
-        vec3 H = normalize(V + L);
-
-
-        vec3 radiance = vec3(directionalData[i].diffuse);
-
-
-        float NDF = DistributionGGX(N, H, roughness);
-        float G = GeometrySmith(N, V, L, roughness);
-        vec3 F = fresnelSchlick(max(dot(H, V), 0.0), F0);
-
-        vec3 numerator = NDF * G * F;
-        float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.0001;
-        vec3 specular = numerator / denominator;
-
-        vec3 kS = F;
-
-        vec3 kD = vec3(1.0) - kS;
-
-        kD *= 1.0 - metallic;
-
-        float NdotL = max(dot(N, L), 0.0);
-
-        if (directionalData[i].padding.x < 1.0) {
-            Lo += (kD * albedo / PI + specular) * radiance * NdotL;
-        }
-        else {
-            Lo += (kD * albedo / PI + specular) * radiance * NdotL * (1 - ShadowCalculationDirectional(FragPos, L, N, i));
-        }
-    }
-
-    // Locating which cluster this fragment is part of
-    uint zTile = uint((log(abs(vec3(view * vec4(FragPos, 1.0)).z) / zNear) * gridSize.z) / log(zFar / zNear));
-    vec2 tileSize = screenDimensions.xy / gridSize.xy;
-    uvec3 tile = uvec3(gl_FragCoord.xy / tileSize, zTile);
-    uint tileIndex =
-        tile.x + (tile.y * gridSize.x) + (tile.z * gridSize.x * gridSize.y);
-
-    uint lightCount = clusters[tileIndex].count;
-
-    for (int i = 0; i < lightCount; i++) {
-        uint lightIndex = clusters[tileIndex].lightIndices[i];
-
-        vec3 distance = vec3(omniData[lightIndex].position) - FragPos;
-        vec3 L = normalize(distance);
-        vec3 H = normalize(V + L);
-
-        float totalDistance = length(distance);
-
-        if (totalDistance <= omniData[lightIndex].radius) {
-
-            vec3 radiance = vec3(omniData[lightIndex].diffuse);
-
-
-            float NDF = DistributionGGX(N, H, roughness);
-            float G = GeometrySmith(N, V, L, roughness);
-            vec3 F = fresnelSchlick(max(dot(H, V), 0.0), F0);
-
-            vec3 numerator = NDF * G * F;
-            float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.0001;
-            vec3 specular = numerator / denominator;
-
-            vec3 kS = F;
-
-            vec3 kD = vec3(1.0) - kS;
-
-            kD *= 1.0 - metallic;
-
-            float NdotL = max(dot(N, L), 0.0);
-
-            float attenuation = 1.0 / (omniData[lightIndex].attenuation.x + omniData[lightIndex].attenuation.y * totalDistance + omniData[lightIndex].attenuation.z * totalDistance * totalDistance);
-
-            if (omniData[lightIndex].padding.x < 1.0) {
-                Lo += (kD * albedo / PI + specular) * radiance * NdotL * attenuation;
-            }
-            else {
-                Lo += (kD * albedo / PI + specular) * radiance * NdotL * attenuation * (1 - ShadowCalculation(FragPos, vec3(omniData[lightIndex].position), int(lightIndex)));
-            }
-
-        }
-    }
-
-    vec3 F = fresnelSchlickRoughness(max(dot(N, V), 0.0), F0, roughness);
-
-    vec3 kS = F;
-    vec3 kD = 1.0 - kS;
-    kD *= 1.0 - metallic;
-
-    vec3 irradiance = texture(irradianceMap, N).rgb;
-    vec3 diffuse = irradiance * albedo;
-
-    const float MAX_REFLECTION_LOD = 4.0;
-    vec3 prefilteredColor = textureLod(prefilterMap, R, roughness * MAX_REFLECTION_LOD).rgb;
-    vec2 brdf = texture(brdfLUT, vec2(max(dot(N, V), 0.0), roughness)).rg;
-    vec3 specular = prefilteredColor * (F * brdf.x + brdf.y);
-
-    vec3 ambient = kD * diffuse + specular;
-    Lo = ambient + Lo;
-
-    FragColor = vec4(Lo,1.0);
+    vec3 pbrColor = pbrCalculation(FragPos, N, blendedColor.xyz, vec4(1.0),blendedRoughness,0.0);
+    FragColor = vec4(pbrColor, 1.0);
 }
