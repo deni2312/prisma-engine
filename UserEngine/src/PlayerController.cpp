fbe7e259d0b0866482d35848aee3e875c228e14c
denis <denis@DENI>
2025-02-25 20:03:07 +0100

    Added groups

--- UserEngine/src/PlayerController.cpp
+++ UserEngine/src/PlayerController.cpp
@@ -52,7 +52,6 @@ PlayerController::PlayerController(std::shared_ptr<Prisma::Scene> scene) : m_sce
 	blackTexture.loadTexture({"../../../Resources/res/black.png", false, true, true, true});
 	terrainComponent->heightMap(*perlin);
 	terrain->addComponent(terrainComponent);
-	m_scene->root->addChild(terrain);
 
 
 	auto contact = [&](const Body& body)
