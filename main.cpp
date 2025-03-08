6ae200ee290a9b8ad72bf810dd34a42c0c82bf1d
denis <denis@DENI>
2025-03-08 01:18:23 +0100

    Added engine

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "UserEngine/include/UserEngine.h"
+#include <windows.h>
 
-int main()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	auto userEngine = std::make_shared<UserEngine>();
 	Prisma::Engine::getInstance().setUserEngine(userEngine);
 	Prisma::Engine::getInstance().run();
+	return 0;
 }
