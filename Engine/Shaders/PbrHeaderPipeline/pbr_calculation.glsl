4bbdd71628103fb7ad8876f9f2f50dd3d31763e4
Denis Beqiraj <denisbeqi2312@gmail.com>
2025-03-30 18:40:34 +0200

    Added includes

--- Engine/Shaders/PbrHeaderPipeline/pbr_calculation.glsl
+++ Engine/Shaders/PbrHeaderPipeline/pbr_calculation.glsl
@@ -1,3 +1,146 @@
+#extension GL_EXT_nonuniform_qualifier : require
+
+uniform ConstantsClusters
+{
+    float zNear;
+    float zFar;
+    vec2 padding2;
+    mat4 inverseProjection;
+    ivec4 gridSize;
+    ivec4 screenDimensions;
+};
+
+const float PI = 3.14159265359;
+
+layout(location = 0) out vec4 FragColor;
+
+
+layout(location = 0) in vec2 outUv;
+layout(location = 1) in vec3 outFragPos; // Fragment position in world space
+layout(location = 2) in vec3 outNormal;
+layout(location = 3) in flat int outDrawId;
+
+
+uniform texture2D diffuseTexture[];
+
+uniform sampler textureClamp_sampler;
+uniform sampler textureRepeat_sampler;
+
+uniform texture2D normalTexture[];
+
+uniform texture2D rmTexture[];
+
+uniform texture2D lut;
+uniform textureCube irradiance;
+uniform textureCube prefilter;
+
+struct OmniData
+{
+    vec4 position;
+    vec4 diffuse;
+    vec4 specular;
+    vec4 far_plane;
+    vec4 attenuation;
+    vec2 depthMap;
+    float padding;
+    float radius;
+};
+
+uniform ViewProjection
+{
+    mat4 view;
+    mat4 projection;
+    vec4 viewPos;
+};
+
+uniform LightSizes
+{
+    int omniSize;
+    int dirSize;
+    int areaSize;
+    int padding;
+};
+
+struct Cluster
+{
+    vec4 minPoint;
+    vec4 maxPoint;
+    uint count;
+    uint lightIndices[100];
+};
+
+readonly buffer omniData{
+    OmniData omniData_data[];
+};
+
+layout(binding=0) buffer clusters{
+    Cluster clusters_data[];
+};
+
+
+
+float DistributionGGX(vec3 N, vec3 H, float roughness)
+{
+    float a = roughness * roughness;
+    float a2 = a * a;
+    float NdotH = max(dot(N, H), 0.0);
+    float NdotH2 = NdotH * NdotH;
+
+    float nom = a2;
+    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    denom = PI * denom * denom;
+
+    return nom / denom;
+}
+
+float GeometrySchlickGGX(float NdotV, float roughness)
+{
+    float r = (roughness + 1.0);
+    float k = (r * r) / 8.0;
+
+    float nom = NdotV;
+    float denom = NdotV * (1.0 - k) + k;
+
+    return nom / denom;
+}
+
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
+{
+    float NdotV = max(dot(N, V), 0.0);
+    float NdotL = max(dot(N, L), 0.0);
+    float ggx2 = GeometrySchlickGGX(NdotV, roughness);
+    float ggx1 = GeometrySchlickGGX(NdotL, roughness);
+
+    return ggx1 * ggx2;
+}
+
+vec3 fresnelSchlick(float cosTheta, vec3 F0)
+{
+    return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
+}
+
+vec3 fresnelSchlickRoughness(float cosTheta, vec3 F0, float roughness)
+{
+    return F0 + (max(vec3(1.0 - roughness), F0) - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
+}
+
+vec3 getNormalFromMap()
+{
+    vec3 tangentNormal = texture(sampler2D(normalTexture[nonuniformEXT(outDrawId)],textureRepeat_sampler),outUv).xyz * 2.0 - 1.0;
+
+    vec3 Q1 = dFdx(outFragPos);
+    vec3 Q2 = dFdy(outFragPos);
+    vec2 st1 = dFdx(outUv);
+    vec2 st2 = dFdy(outUv);
+
+    vec3 N = normalize(outNormal);
+    vec3 T = normalize(Q1 * st2.t - Q2 * st1.t);
+    vec3 B = -normalize(cross(N, T));
+    mat3 TBN = mat3(T, B, N);
+
+    return normalize(TBN * tangentNormal);
+}
+
 vec3 pbrCalculation(vec3 FragPos, vec3 N, vec3 albedo, vec4 aoSpecular,float roughness,float metallic) {
 
     float specularMap = aoSpecular.r;
@@ -12,100 +155,29 @@ vec3 pbrCalculation(vec3 FragPos, vec3 N, vec3 albedo, vec4 aoSpecular,float rou
 
     vec3 R = reflect(-V, N);
 
-    for (int i = 0; i < lenDir.r; i++) {
-
-        vec3 L = normalize(vec3(directionalData[i].direction));
-        vec3 H = normalize(V + L);
-
-
-        vec3 radiance = vec3(directionalData[i].diffuse);
-
-
-        float NDF = DistributionGGX(N, H, roughness);
-        float G = GeometrySmith(N, V, L, roughness);
-        vec3 F = fresnelSchlick(max(dot(H, V), 0.0), F0);
-
-        vec3 numerator = NDF * G * F;
-        float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.0001;
-        vec3 specular = numerator / denominator;
-
-        vec3 kS = F * specularMap;
-
-        vec3 kD = vec3(1.0) - kS;
-
-        kD *= 1.0 - metallic;
-
-        float NdotL = max(dot(N, L), 0.0);
-
-        if (directionalData[i].padding.x < 1.0) {
-            Lo += (kD * albedo / PI + specular) * radiance * NdotL;
-        }
-        else {
-            Lo += (kD * albedo / PI + specular) * radiance * NdotL * (1 - ShadowCalculationDirectional(FragPos, L, N, i));
-        }
-    }
-
-    float dotNV = clamp(dot(N, V), 0.0f, 1.0f);
-
-    // use roughness and sqrt(1-cos_theta) to sample M_texture
-    vec2 uv = vec2(roughness, sqrt(1.0f - dotNV));
-    uv = uv*LUT_SCALE + LUT_BIAS;
-
-    // get 4 parameters for inverse_M
-    vec4 t1 = texture(textureM, uv);
-
-    // Get 2 parameters for Fresnel calculation
-    vec4 t2 = texture(textureLut, uv);
-
-    mat3 Minv = mat3(
-        vec3(t1.x, 0, t1.y),
-        vec3(  0,  1,    0),
-        vec3(t1.z, 0, t1.w)
-    );
-    for (int i = 0; i < lenArea.r; i++) {
-        		// Evaluate LTC shading
-
-        vec3 position[4];
-        position[0]=vec3(areaData[i].position[0]);
-        position[1]=vec3(areaData[i].position[1]);
-        position[2]=vec3(areaData[i].position[2]);
-        position[3]=vec3(areaData[i].position[3]);
-
-        vec3 mSpecular = ToLinear(vec3(areaData[i].diffuse)); // mDiffuse
-        bool doubleSide=areaData[i].doubleSide>0;
-		vec3 diffuse = LTC_Evaluate(N, V, FragPos, mat3(1), position, doubleSide);
-		vec3 specular = LTC_Evaluate(N, V, FragPos, Minv, position, doubleSide);
-
-		// GGX BRDF shadowing and Fresnel
-		// t2.x: shadowedF90 (F90 normally it should be 1.0)
-		// t2.y: Smith function for Geometric Attenuation Term, it is dot(V or L, H).
-		specular *= mSpecular*t2.x + (1.0f - mSpecular) * t2.y;
-
-		// Add contribution
-		Lo += vec3(areaData[i].diffuse) * (specular + albedo * diffuse);
-    }
-
     // Locating which cluster this fragment is part of
     uint zTile = uint((log(abs(vec3(view * vec4(FragPos, 1.0)).z) / zNear) * gridSize.z) / log(zFar / zNear));
     vec2 tileSize = screenDimensions.xy / gridSize.xy;
-    uvec3 tile = uvec3(gl_FragCoord.xy / tileSize, zTile);
+
+    vec2 fragCoord=gl_FragCoord.xy;
+    uvec3 tile = uvec3(fragCoord / tileSize, zTile);
     uint tileIndex =
         tile.x + (tile.y * gridSize.x) + (tile.z * gridSize.x * gridSize.y);
 
-    uint lightCount = clusters[tileIndex].count;
+    uint lightCount = clusters_data[tileIndex].count;
 
     for (int i = 0; i < lightCount; i++) {
-        uint lightIndex = clusters[tileIndex].lightIndices[i];
+        uint lightIndex = clusters_data[tileIndex].lightIndices[i];
 
-        vec3 distance = vec3(omniData[lightIndex].position) - FragPos;
+        vec3 distance = vec3(omniData_data[lightIndex].position) - FragPos;
         vec3 L = normalize(distance);
         vec3 H = normalize(V + L);
 
         float totalDistance = length(distance);
 
-        if (totalDistance <= omniData[lightIndex].radius) {
+        if (totalDistance <= omniData_data[lightIndex].radius) {
 
-            vec3 radiance = vec3(omniData[lightIndex].diffuse);
+            vec3 radiance = vec3(omniData_data[lightIndex].diffuse);
 
 
             float NDF = DistributionGGX(N, H, roughness);
@@ -124,13 +196,13 @@ vec3 pbrCalculation(vec3 FragPos, vec3 N, vec3 albedo, vec4 aoSpecular,float rou
 
             float NdotL = max(dot(N, L), 0.0);
 
-            float attenuation = 1.0 / (omniData[lightIndex].attenuation.x + omniData[lightIndex].attenuation.y * totalDistance + omniData[lightIndex].attenuation.z * totalDistance * totalDistance);
+            float attenuation = 1.0 / (omniData_data[lightIndex].attenuation.x + omniData_data[lightIndex].attenuation.y * totalDistance + omniData_data[lightIndex].attenuation.z * totalDistance * totalDistance);
 
-            if (omniData[lightIndex].padding.x < 1.0) {
+            if (omniData_data[lightIndex].padding.x < 1.0) {
                 Lo += (kD * albedo / PI + specular) * radiance * NdotL * attenuation;
             }
             else {
-                Lo += (kD * albedo / PI + specular) * radiance * NdotL * attenuation * (1 - ShadowCalculation(FragPos, vec3(omniData[lightIndex].position), int(lightIndex)));
+                //Lo += (kD * albedo / PI + specular) * radiance * NdotL * attenuation * (1 - ShadowCalculation(FragPos, vec3(omniData_data[lightIndex].position), int(lightIndex)));
             }
 
         }
@@ -142,12 +214,12 @@ vec3 pbrCalculation(vec3 FragPos, vec3 N, vec3 albedo, vec4 aoSpecular,float rou
     vec3 kD = 1.0 - kS;
     kD *= 1.0 - metallic;
 
-    vec3 irradiance = texture(irradianceMap, N).rgb;
+    vec3 irradiance = texture(samplerCube(irradiance,textureClamp_sampler), N).rgb;
     vec3 diffuse = irradiance * albedo;
 
-    const float MAX_REFLECTION_LOD = 4.0;
-    vec3 prefilteredColor = textureLod(prefilterMap, R, roughness * MAX_REFLECTION_LOD).rgb;
-    vec2 brdf = texture(brdfLUT, vec2(max(dot(N, V), 0.0), roughness)).rg;
+    const float MAX_REFLECTION_LOD = 8.0;
+    vec3 prefilteredColor = textureLod(samplerCube(prefilter,textureClamp_sampler), R, roughness * MAX_REFLECTION_LOD).rgb;
+    vec2 brdf = texture(sampler2D(lut,textureClamp_sampler), vec2(max(dot(N, V), 0.0), roughness)).rg;
     vec3 specular = prefilteredColor * (F * brdf.x + brdf.y);
 
     vec3 ambient = (kD * diffuse + specular) * ao;
