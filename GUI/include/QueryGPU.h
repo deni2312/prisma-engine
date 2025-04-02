b415e972c40b73b12b671c3d9be6a2042933b682
deni <deni>
2025-04-02 15:27:36 +0200

    Added query

--- GUI/include/QueryGPU.h
+++ GUI/include/QueryGPU.h
@@ -1,17 +1,36 @@
 #pragma once
 #include "GlobalData/InstanceData.h"
+#include "GlobalData/Platform.h"
+#include "Graphics/GraphicsEngine/interface/Query.h"
+#include "Graphics/GraphicsTools/interface/DurationQueryHelper.hpp"
+#include "Graphics/GraphicsTools/interface/ScopedQueryHelper.hpp"
 
 namespace Prisma {
-	class QueryGPU : InstanceData<QueryGPU>{
+	class QueryGPU : public InstanceData<QueryGPU> {
 	public:
 		struct QueryData {
-			unsigned int numTriangles;
+			Diligent::QueryDataPipelineStatistics PipelineStats;
+			Diligent::QueryDataOcclusion          Occlusion;
+			Diligent::QueryDataDuration           Duration;
+			double                                TimestampBegin;
+			double                                TimestampEnd;
+			float                                 TimestampFrequency;
 		};
 
-		void start();
+		QueryGPU();
 
+		void start();
 		const QueryData& end();
+
 	private:
-		QueryData m_data;
+		std::unique_ptr<Diligent::ScopedQueryHelper>   m_pPipelineStatsQuery;
+		std::unique_ptr<Diligent::ScopedQueryHelper>   m_pOcclusionQuery;
+		std::unique_ptr<Diligent::ScopedQueryHelper>   m_pDurationQuery;
+		std::unique_ptr<Diligent::ScopedQueryHelper>   m_pTimestampQueryBegin;
+		std::unique_ptr<Diligent::ScopedQueryHelper>   m_pTimestampQueryEnd;
+		std::unique_ptr<Diligent::DurationQueryHelper> m_pDurationFromTimestamps;
+		double m_DurationFromTimestamps = 0;
+
+		QueryData m_QueryData;
 	};
 }
--- GUI/src/QueryGPU.cpp
+++ GUI/src/QueryGPU.cpp
@@ -1,10 +1,71 @@
 #include "../include/QueryGPU.h"
 
+#include "GlobalData/PrismaFunc.h"
+#include "Graphics/GraphicsEngine/interface/Query.h"
+#include "Graphics/GraphicsTools/interface/ScopedQueryHelper.hpp"
+
+Prisma::QueryGPU::QueryGPU()
+{
+    auto& contextData = Prisma::PrismaFunc::getInstance().contextData();
+
+    // Check query support
+    const auto& Features = contextData.m_pDevice->GetDeviceInfo().Features;
+    if (Features.PipelineStatisticsQueries)
+    {
+        Diligent::QueryDesc queryDesc;
+        queryDesc.Name = "Pipeline statistics query";
+        queryDesc.Type = Diligent::QUERY_TYPE_PIPELINE_STATISTICS;
+        m_pPipelineStatsQuery.reset(new Diligent::ScopedQueryHelper{ contextData.m_pDevice, queryDesc, 2 });
+    }
+
+    if (Features.OcclusionQueries)
+    {
+        Diligent::QueryDesc queryDesc;
+        queryDesc.Name = "Occlusion query";
+        queryDesc.Type = Diligent::QUERY_TYPE_OCCLUSION;
+        m_pOcclusionQuery.reset(new Diligent::ScopedQueryHelper{ contextData.m_pDevice, queryDesc, 2 });
+    }
+
+    if (Features.DurationQueries)
+    {
+        Diligent::QueryDesc queryDesc;
+        queryDesc.Name = "Duration query";
+        queryDesc.Type = Diligent::QUERY_TYPE_DURATION;
+        m_pDurationQuery.reset(new Diligent::ScopedQueryHelper{ contextData.m_pDevice, queryDesc, 2 });
+    }
+
+    if (Features.TimestampQueries)
+    {
+        m_pDurationFromTimestamps.reset(new Diligent::DurationQueryHelper{ contextData.m_pDevice, 2 });
+    }
+}
+
 void Prisma::QueryGPU::start()
 {
+    auto immediateContext = Prisma::PrismaFunc::getInstance().contextData().m_pImmediateContext;
+    // Begin supported queries
+    if (m_pPipelineStatsQuery)
+        m_pPipelineStatsQuery->Begin(immediateContext);
+    if (m_pOcclusionQuery)
+        m_pOcclusionQuery->Begin(immediateContext);
+    if (m_pDurationFromTimestamps)
+        m_pDurationFromTimestamps->Begin(immediateContext);
+    if (m_pDurationQuery)
+        m_pDurationQuery->Begin(immediateContext);
 }
 
 const Prisma::QueryGPU::QueryData& Prisma::QueryGPU::end()
 {
-	return QueryData();
+    auto immediateContext = Prisma::PrismaFunc::getInstance().contextData().m_pImmediateContext;
+
+    if (m_pDurationFromTimestamps)
+        m_pDurationFromTimestamps->End(immediateContext, m_DurationFromTimestamps);
+    if (m_pDurationQuery)
+        m_pDurationQuery->End(immediateContext, &m_QueryData.Duration, sizeof(m_QueryData.Duration));
+    if (m_pOcclusionQuery)
+        m_pOcclusionQuery->End(immediateContext, &m_QueryData.Occlusion, sizeof(m_QueryData.Occlusion));
+    if (m_pPipelineStatsQuery)
+        m_pPipelineStatsQuery->End(immediateContext, &m_QueryData.PipelineStats, sizeof(m_QueryData.PipelineStats));
+
+    return m_QueryData;
 }
