e13ce381c477a5378f130f14ddcdded0de9a3233
Denis Beqiraj <denisbeqi2312@gmail.com>
2025-04-03 22:20:11 +0200

    Added geometry

--- Engine/Shaders/OmniShadowPipeline/geometry.glsl
+++ Engine/Shaders/OmniShadowPipeline/geometry.glsl
@@ -14,7 +14,8 @@ void main()
         gl_Layer = face; // built-in variable that specifies to which face we render.
         for(int i = 0; i < 3; ++i) // for each triangle's vertices
         {
-            FragPos = gl_in[i].gl_Position;
+            vec4 FragPos1=vec4(gl_in[i].gl_Position.x,gl_in[i].gl_Position.y,gl_in[i].gl_Position.z,gl_in[i].gl_Position.w);
+            FragPos = FragPos1;
             gl_Position = shadowMatrices[face] * gl_in[i].gl_Position;
             EmitVertex();
         }
