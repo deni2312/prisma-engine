0b4d3c0eb39b4be9fe5e0bc4b1fda8ac00b0e3ff
Denis Beqiraj <denisbeqi2312@gmail.com>
2025-04-10 15:21:24 +0200

    Added shaders cleanup

--- Engine/Shaders/CSMPipeline/vertex.glsl
+++ Engine/Shaders/CSMPipeline/vertex.glsl
@@ -1,10 +1,6 @@
 #extension GL_ARB_shader_draw_parameters : enable
 
 layout(location = 0) in vec3 inPos;
-layout(location = 1) in vec3 inNormal;
-layout(location = 2) in vec2 inUV;
-layout(location = 3) in vec3 inTangent;
-layout(location = 4) in vec3 inBiTangent;
 
 struct MeshData
 {
