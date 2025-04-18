8bea1aebe105da750d96d90e744278b472ee3c2f
Denis Beqiraj <denisbeqi2312@gmail.com>
2025-04-18 14:17:33 +0200

    Added vulkans

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, const char* const* argv)
 {
-	//_CrtSetBreakAlloc(169714);
+	//_CrtSetBreakAlloc(1714);
 	auto userEngine = std::make_shared<UserEngine>();
 	Prisma::Engine::getInstance().setUserEngine(userEngine);
 	Prisma::Engine::getInstance().setGuiData(Prisma::ImguiDebug::getInstance().handlers());
